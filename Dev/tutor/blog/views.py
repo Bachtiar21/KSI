cf4821339bc03385795173019f2b73a11f094307
Bachtiar21 <bachtiarramadhan26@gmail.com>
2022-11-02 18:10:41 +0700

    first commit

---
+++ Dev/tutor/blog/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+
+def index(request):
+    context = {
+        'Judul': 'blog1',
+        'h1': 'Django',
+        'menu': [['blog/', 'Home'], ['blog/recent'], ['/post', 'Post']]
+    }
+    return render(request, 'blog/index.html', context)
+
+
+def recent(request):
+    # context = {
+    #     'Judul' : 'Identitas Diri',
+    #     'h1' : 'Bachtiar Ramadhan',
+    #     'h2' : '21 Desember 2000',
+    #     'h3' : 'Pekanbaru, Riau'
+    # }
+    # return render(request, 'blog/index.html', context)
+
+    return HttpResponse("RECENT BLOG")
